7088a5a695bad0359a098cf65687869a7cc310ec
Vivid-Vortex <ctntc9.dpk@gmail.com>
2024-12-24 15:38:28 +0530

    init commit

---
+++ ReactorBusPoc/src/test/java/com/event/bus/ReactorBusPoc/ReactorBusPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.event.bus.ReactorBusPoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactorBusPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
