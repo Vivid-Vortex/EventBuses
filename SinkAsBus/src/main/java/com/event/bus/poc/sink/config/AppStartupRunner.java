1aa45273ef13baee789bdf00c14e851bbc3d6663
Vivid-Vortex <ctntc9.dpk@gmail.com>
2025-01-01 19:54:44 +0530

    remove duplicate AppStartupRunner

--- SinkAsBus/src/main/java/com/event/bus/poc/sink/config/AppStartupRunner.java
+++ SinkAsBus/src/main/java/com/event/bus/poc/sink/config/AppStartupRunner.java
@@ -1,21 +1,21 @@
-package com.event.bus.poc.sink.config;
-
-import com.event.bus.poc.sink.bus.EventProcessor;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
-
-@Component
-public class AppStartupRunner implements CommandLineRunner {
-
-    private final EventProcessor processor;
-
-    public AppStartupRunner(EventProcessor processor) {
-        this.processor = processor;
-    }
-
-    @Override
-    public void run(String... args) {
-        processor.startProcessing();
-    }
-}
-
+//package com.event.bus.poc.sink.config;
+//
+//import com.event.bus.poc.sink.bus.EventProcessor;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class AppStartupRunner implements CommandLineRunner {
+//
+//    private final EventProcessor processor;
+//
+//    public AppStartupRunner(EventProcessor processor) {
+//        this.processor = processor;
+//    }
+//
+//    @Override
+//    public void run(String... args) {
+//        processor.startProcessing();
+//    }
+//}
+//
